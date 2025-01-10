639a10d8bdadd7766c61467ded67724a2dc97a32
berfinkaracay <berfinkaracay@gmail.com>
2025-01-11 02:57:37 +0300

    başlık 2

--- project.py
+++ project.py
@@ -4,8 +4,11 @@ import pandas as pd
 # Başlık
 st.title("Hypothesis Testing App")
 
+# Boşluk ve stil ayarları
+st.markdown("<div style='margin-top: 30px; text-align: center; font-size: 20px;'>Upload a CSV file</div>", unsafe_allow_html=True)
+
 # CSV dosyası yükleme
-uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
+uploaded_file = st.file_uploader("", type=["csv"])
 if uploaded_file:
     data = pd.read_csv(uploaded_file)
     st.write("## Dataset Preview")
