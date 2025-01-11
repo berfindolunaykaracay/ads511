1946446629959190d79c9d0518021410c40efdc5
berfinkaracay <berfinkaracay@gmail.com>
2025-01-11 03:02:01 +0300

    eski

--- project.py
+++ project.py
@@ -1,38 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-# Sayfa stil ayarları
-st.markdown(
-    """
-    <style>
-        body {
-            background-color: white;
-            color: #002366;
-        }
-        h1 {
-            color: #002366;
-            text-align: center;
-        }
-        .stButton button {
-            background-color: #002366;
-            color: white;
-            border-radius: 5px;
-            font-size: 16px;
-        }
-        .stFileUploader {
-            margin-top: 30px;
-            text-align: center;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 # Başlık
-st.markdown("<h1>Hypothesis Testing App</h1>", unsafe_allow_html=True)
+st.title("Hypothesis Testing App")
 
 # Boşluk ve stil ayarları
-st.markdown("<div style='margin-top: 30px; text-align: center; font-size: 20px;'>Upload a CSV file</div>", unsafe_allow_html=True)
+st.write("\n\n")
+st.header("Upload a CSV file")
 
 # CSV dosyası yükleme
 uploaded_file = st.file_uploader("", type=["csv"])
