be5e63d9749eaf351d9db4c0567bf68bcb2539a4
berfinkaracay <berfinkaracay@gmail.com>
2025-01-11 03:02:37 +0300

    ortala

--- project.py
+++ project.py
@@ -2,15 +2,14 @@ import streamlit as st
 import pandas as pd
 
 # Başlık
-st.title("Hypothesis Testing App")
+st.markdown("<h1 style='text-align: center;'>Hypothesis Testing App</h1>", unsafe_allow_html=True)
 
 # Boşluk ve stil ayarları
-st.write("\n\n")
-st.header("Upload a CSV file")
+st.markdown("<h2 style='text-align: center;'>Upload a CSV file</h2>", unsafe_allow_html=True)
 
 # CSV dosyası yükleme
 uploaded_file = st.file_uploader("", type=["csv"])
 if uploaded_file:
     data = pd.read_csv(uploaded_file)
-    st.write("## Dataset Preview")
+    st.markdown("<h2 style='text-align: center;'>Dataset Preview</h2>", unsafe_allow_html=True)
     st.write(data.head())
